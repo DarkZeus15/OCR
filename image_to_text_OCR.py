acbf3b358c88fde0e67d8095ef1628c0a0418b9c
Ajinkya B <63950711+DarkZeus15@users.noreply.github.com>
2022-02-23 15:03:09 +0530

    Update image_to_text_OCR.py
    Updated

--- image_to_text_OCR.py
+++ image_to_text_OCR.py
@@ -4,7 +4,7 @@ from PIL import Image
 from pytesseract import pytesseract as ptr
 
 #Load the path of tesserract exe, Install tesseract.exe on your device and get the path from installed location
-path_to_tesseract = r"C:\\Program Files (x86)\\Tesseract-OCR\tesseract.exe"
+path_to_tesseract = r"C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
 
 #Get image path, Read mode
 image_path = r"C:\\Users\\Dark Zeus\\OneDrive\\Documents\\150 pages set (1021)\\150 pages set (1021)\\1.jpg"
